4cdf358e7eaea5deda641ed06c4787856d177bb6
Bartosz Michalak <111230986+BartoszMichalak13@users.noreply.github.com>
2022-08-18 18:58:16 +0200

    Add files via upload

---
+++ Java/Lista0/Zad2/parametry.java
@@ -0,0 +1,14 @@
+package lista0.zad2;
+public class parametry
+{
+    public static void main(String[] args)
+    {
+        
+        int len=args.length;
+        
+        for(int i=0; i<len; i++)
+        {
+            System.out.println(args[i]);
+        }
+    }
+}
